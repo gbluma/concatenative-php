bbcfb903b2906f2340df5274a01cf600fb16af47
Garrett Bluma <gbluma@deltadentalwi.com>
2013-10-22 22:19:08 -0500

    Added new parser.. and it's soo much better

--- new_parser.php
+++ new_parser.php
@@ -6,25 +6,40 @@ $funcs = array();
 $stack = array();
 $in_definition = false;
 
-function process($word) { global $stack, $funcs; if (isset($funcs[$word])) $funcs[$word](); }
+function process($word) { 
+    global $stack, $funcs, $in_definition; 
+    if ($word == ';' or !$in_definition) 
+        if (isset($funcs[$word])) $funcs[$word](); 
+}
 function push($x) { global $stack, $funcs; return array_push($stack, $x); }
 function pop() { global $stack, $funcs; return array_pop($stack); }
-function pop_back_to($x) { global $stack, $funcs; 
+function pop_back_to($x) { 
+    global $stack, $funcs; 
     $words = array();
     while( count($stack) > 0 ) {
         $word = pop();
-        if ($word === $x) { return $words; } 
+        if ($word === $x) { return array_reverse($words); } 
         else { $words[] = $word; }
     }
-    return $words;
+    return array_reverse($words);
 }
 
 function read($str) {
+    $squote = $dquote = false;
     $word = '';
     foreach(str_split($str) as $c) {
         switch($c) {
-            case ' ': if (!empty($word)) { push($word); process($word); } $word = ''; break;
-            default:  $word .= $c;
+            case " ": 
+            case "\r": 
+            case "\n": 
+                if (!$squote && !$dquote) {
+                    if (!empty($word)) { push($word); process($word); } 
+                    $word = ''; 
+                } else { $word .= $c; }
+                break;
+            case "'": $squote = !$squote; $word .= $c; break;
+            case '"': $dquote = !$dquote; $word .= $c; break;
+            default: $word .= $c;
         }
     }
     if (!empty($word)) { push($word); process($word); }
@@ -35,20 +50,21 @@ function read($str) {
 $funcs[']'] = function() { 
     global  $in_definition;
     pop(); 
-    if (!$in_definition) {
-        $words = pop_back_to('[');
-        push(function() use ($words) { return read(implode(" ", $words)); });
-    }
+    $words = pop_back_to('[');
+    push(function() use ($words) { return read(implode(" ", $words)); });
 };
 $funcs[':'] = function() { global $in_definition; $in_definition = true; };
 $funcs[';'] = function() { 
     global $funcs, $in_definition;
     pop(); 
     $words = pop_back_to(':');
-    $name = array_pop($words);
+    $name = array_shift($words);
     $funcs[$name] = function() use ($words){ pop(); return read(implode(" ", $words)); };
     $in_definition = false;
 };
+
+$funcs[')'] = function() { pop(); $words = pop_back_to('('); };
+$funcs['clear'] = function() { global $stack; $stack = array(); };
 $funcs['call'] = function() { pop(); $a = pop(); $a(); };
 $funcs['swap'] = function() { pop(); $a = pop(); $b = pop(); push($a); push($b); };
 $funcs['dup'] = function() { pop(); $a = pop(); push($a); push($a); };
@@ -56,7 +72,21 @@ $funcs['+'] = function() { pop(); push(pop() + pop()); };
 $funcs['-'] = function() { pop(); $a = pop(); $b = pop(); push($b - $a); };
 $funcs['*'] = function() { pop(); push(pop() * pop()); };
 $funcs['/'] = function() { pop(); $a = pop(); $b = pop(); push($b / $a); };
+$funcs['.'] = function() { pop(); echo pop(); };
 
-read(": a [ 5 2 / ] ;");
-read("a");
-var_dump($stack);
+
+// start repl
+while(true) {
+    echo ">>> ";
+    $handle = fopen ("php://stdin","r");
+    $line = fgets($handle);
+    read($line);
+    echo "\n";
+}
+
+
+
+
+
+
+
