7c64324a5fd6dd8dcbf3a05b025cb9d5cfccb4e0
Garrett Bluma <gbluma@deltadentalwi.com>
2013-10-29 20:27:10 -0500

    Adding more tests.

--- tests/CoreLanguageTest.php
+++ tests/CoreLanguageTest.php
@@ -8,6 +8,11 @@ require_once("src/Concatenative/language.php");
  */
 class CoreLanguageTest extends \PHPUnit_Framework_TestCase 
 {
+	public function tearDown()
+	{
+    	read("clear");
+	}
+
     public function test_function_definition_cleans_up_stack()
     {
         global $stack;
@@ -22,13 +27,66 @@ class CoreLanguageTest extends \PHPUnit_Framework_TestCase
         $this->assertEmpty($stack);
     }
 
+    public function test_tokenization() 
+    {
+        global $stack;
+
+        $this->assertEquals( 0, count($stack) );
+        read("3"); 
+        $this->assertEquals( 1, count($stack) );
+        read("3"); 
+        $this->assertEquals( 2, count($stack) );
+        read("3 3"); 
+        $this->assertEquals( 4, count($stack) );
+    }
+
+    public function test_that_clear_works() 
+    {
+        global $stack;
+        read("3 3"); 
+        $this->assertEquals( 2, count($stack) );
+        read("clear");
+        $this->assertEquals( 0, count($stack) );
+    }
+
     public function test_function_exists_after_definition() 
     {
         global $stack, $funcs;
 
         $this->assertFalse( isset($funcs['testing']) );
         read(": testing ( -- ) 3 ;"); 
-        $this->assertTrue(  isset($funcs['testing']) );
+        $this->assertTrue( isset($funcs['testing']) );
+    }
+    
+    public function test_function_application() 
+    {
+    	global $stack; 
+		read(": square ( -- ) dup * ; ");
+		read("15 square");
+    	$this->assertTrue(count($stack) === 1);
+    	$this->assertEquals(225, $stack[0]);
+    }
+    
+    public function test_stack_is_still_empty_after_teardown()
+    {
+    	global $stack; 	
+    	$this->assertEmpty($stack);
+    }
+
+    public function test_that_quotation_is_callable()
+    {
+    	global $stack; 	
+    	read("[ dup dup * * ]");
+    	$this->assertTrue( count($stack) === 1 );
+    	$this->assertTrue( is_callable($stack[0]) );
+    }
+
+    public function test_that_quotation_executionn()
+    {
+    	global $stack; 	
+    	read("3 [ dup dup * * ] call ");
+    	$this->assertTrue( count($stack) === 1 );
+    	$this->assertTrue( $stack[0] === 27 );
     }
 
 }
